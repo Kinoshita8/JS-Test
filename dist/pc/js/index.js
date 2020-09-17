e2aaeb61c46562e151bbcc7bfea3d51a0a8c46c3
Kinoshita <underthetree008@gmail.com>
2020-09-17 17:29:58 +0900

    js タブ遷移 追加

--- dist/pc/js/index.js
+++ dist/pc/js/index.js
@@ -9,8 +9,7 @@
         for (let i = 0; i < $buttons.length; i++) {
             $buttons[i].addEventListener("click", function () {
                 var id = $buttons[i].getAttribute('href');
-                id = id.replace('#', '');
-                var $position = document.getElementById(id);
+                var $position = document.querySelector(id);
                 Velocity($position, 'scroll', {
                     duration: 800,
                     offset: -65,
@@ -25,7 +24,7 @@
     closeModal();
 
     function openModal() {
-        var $modalLink = document.getElementById('js-modal_link');
+        var $modalLink = document.querySelector('#js-modal_link');
         var $overlay = document.querySelector(".overlay");
         var $modalContents = document.querySelector(".modal_contents");
         // モーダルウィンドウを開く
@@ -36,7 +35,7 @@
                 duration: 500
             });
         });
-        //　モーダルコンテンツ内クリック時にモーダルウィンドウが閉じるのを防ぐ（親要素への伝播を止める）
+        //　モーダルコンテンツ内クリック時にモーダルウィンドウが閉じるのを防ぐ
         $modalContents.addEventListener('click', function (e) {
             e.stopPropagation();
         });
@@ -65,10 +64,10 @@
     hamburger();
 
     function hamburger() {
-        var $hamburger = document.getElementById('menu');
+        var $hamburger = document.querySelector('#menu');
         var $hamburgerLine = document.querySelectorAll('span');
         var $menu = document.querySelector('.sub-menu');
-        var $bg = document.getElementById('bg');
+        var $bg = document.querySelector('#bg');
         $hamburger.addEventListener('click', function () {
             for (var i = 0; i < $hamburgerLine.length; i++) {
                 $hamburgerLine[i].classList.toggle('is-active');
@@ -76,5 +75,27 @@
             $menu.classList.toggle('sub-menu-active');
             $bg.classList.toggle('sub-menu__bg');
         });
+    }
+
+
+
+    // タブ遷移
+    tabSwitch();
+
+    function tabSwitch() {
+        var $tabs = document.querySelectorAll('.tab');
+        for (var i = 0; i < $tabs.length; i++) {
+            $tabs[i].addEventListener('click', function () {
+                // タブのclassの値を変更
+                document.querySelector('.is-active').classList.remove('is-active');
+                this.classList.add('is-active');
+                // コンテンツのclassの値を変更
+                document.querySelector('.is-show').classList.remove('is-show');
+                const arrayTabs = Array.prototype.slice.call($tabs); //引数にとったオブジェクトを配列に変換
+                const index = arrayTabs.indexOf(this); //配列に対してだけ使い、DOMを引数にとって一致した要素番号を戻す
+                document.querySelectorAll('.panel')[index].classList.add('is-show');
+            });
+        }
+
     }
 }());
