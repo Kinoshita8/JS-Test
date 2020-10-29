9ac9815630ba7b89c8a0c6d392b01c3222d94e46
Kinoshita <underthetree008@gmail.com>
2020-10-29 10:18:22 +0900

    js修正

--- dist/pc/js/index.js
+++ dist/pc/js/index.js
@@ -7,7 +7,7 @@
     function smoothScroll() {
         var $buttons = document.querySelectorAll('a[href^="#"]');
         for (let i = 0; i < $buttons.length; i++) {
-            $buttons[i].addEventListener("click", function () {
+            $buttons[i].addEventListener('click', function () {
                 var id = $buttons[i].getAttribute('href');
                 var $position = document.querySelector(id);
                 Velocity($position, 'scroll', {
@@ -25,13 +25,13 @@
 
     function openModal() {
         var $modalLink = document.querySelector('#js-modal_link');
-        var $overlay = document.querySelector(".overlay");
-        var $modalContents = document.querySelector(".modal_contents");
+        var $overlay = document.querySelector('.overlay');
+        var $modalContents = document.querySelector('.modal_contents');
         // モーダルウィンドウを開く
         $modalLink.addEventListener('click', function (e) {
             e.preventDefault();
             $overlay.style.display = 'block';
-            Velocity($overlay, "fadeIn", {
+            Velocity($overlay, 'fadeIn', {
                 duration: 500
             });
         });
@@ -42,19 +42,19 @@
     }
 
     function closeModal() {
-        var $close = document.querySelector(".close");
-        var $overlay = document.querySelector(".overlay");
+        var $close = document.querySelector('.close');
+        var $overlay = document.querySelector('.overlay');
         // オーバーレイクリック時オーバーレイを削除
         $overlay.addEventListener('click', function (e) {
             e.preventDefault();
-            Velocity($overlay, "fadeOut", {
+            Velocity($overlay, 'fadeOut', {
                 duration: 100
             });
         });
         // クローズリンククリック時にオーバーレイを削除する
         $close.addEventListener('click', function (e) {
             e.preventDefault();
-            Velocity($overlay, "fadeOut", {
+            Velocity($overlay, 'fadeOut', {
                 duration: 300
             });
         });
@@ -65,7 +65,7 @@
 
     function hamburger() {
         var $hamburger = document.querySelector('#menu');
-        var $hamburgerLine = document.querySelectorAll('span');
+        var $hamburgerLine = document.querySelectorAll('.hamburger_line');
         var $menu = document.querySelector('.sub-menu');
         var $bg = document.querySelector('#bg');
         $hamburger.addEventListener('click', function () {
@@ -82,20 +82,39 @@
     // タブ遷移
     tabSwitch();
 
+    // function tabSwitch() {
+    //     var $tabs = document.querySelectorAll('.tab');
+    //     for (var i = 0; i < $tabs.length; i++) {
+    //         $tabs[i].addEventListener('click', function () {
+    //             // タブのclassの値を変更
+    //             document.querySelector('.is-active').classList.remove('is-active');
+    //             this.classList.add('is-active');
+    //             // コンテンツのclassの値を変更
+    //             document.querySelector('.is-show').classList.remove('is-show');
+    //             const arrayTabs = Array.prototype.slice.call($tabs); //引数にとったオブジェクトを配列に変換
+    //             const index = arrayTabs.indexOf(this); //配列に対してだけ使い、DOMを引数にとって一致した要素番号を戻す
+    //             document.querySelectorAll('.panel')[index].classList.add('is-show');
+    //         });
+    //     }
+
+    // }
+
     function tabSwitch() {
         var $tabs = document.querySelectorAll('.tab');
         for (var i = 0; i < $tabs.length; i++) {
-            $tabs[i].addEventListener('click', function () {
-                // タブのclassの値を変更
-                document.querySelector('.is-active').classList.remove('is-active');
-                this.classList.add('is-active');
-                // コンテンツのclassの値を変更
-                document.querySelector('.is-show').classList.remove('is-show');
-                const arrayTabs = Array.prototype.slice.call($tabs); //引数にとったオブジェクトを配列に変換
-                const index = arrayTabs.indexOf(this); //配列に対してだけ使い、DOMを引数にとって一致した要素番号を戻す
-                document.querySelectorAll('.panel')[index].classList.add('is-show');
-            });
+            (function (n) {
+                $tabs[n].addEventListener('click', function () {
+                    // タブのclassの値を変更
+                    document.querySelector('.is-active').classList.remove('is-active');
+                    this.classList.add('is-active');
+                    // コンテンツのclassの値を変更
+                    document.querySelector('.is-show').classList.remove('is-show');
+                    document.querySelectorAll('.panel')[n].classList.add('is-show');
+                });
+            })(i);
         }
 
     }
+
+
 }());
